1bdf5f8cbe74ff3de02f01eb951557ac9f0bae99
John Healy <johnthealy3@gmail.com>
2012-12-03 14:40:51 -0500

    remove crispy forms from settings

--- ehrgonomic/settings.py
+++ ehrgonomic/settings.py
@@ -118,7 +118,6 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
     'records',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',

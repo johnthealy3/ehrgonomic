fb72f5d07a6f6a6c561ae98b70dca60e52b8aecf
John Healy <johnthealy3@gmail.com>
2012-12-03 14:43:57 -0500

    fix settings

--- ehrgonomic/settings.py
+++ ehrgonomic/settings.py
@@ -66,6 +66,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+  '/app/static',
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -106,6 +107,7 @@ WSGI_APPLICATION = 'ehrgonomic.wsgi.application'
 
 TEMPLATE_DIRS = (
     '/home/john/Documents/code/ehrgonomic/templates',
+    '/app/templates'
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.

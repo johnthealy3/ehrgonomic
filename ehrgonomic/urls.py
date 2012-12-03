2f096ec32fd418fdd4eb00f529e488e135616e76
John Healy <johnthealy3@gmail.com>
2012-12-03 14:27:30 -0500

    add home page and submission

--- ehrgonomic/urls.py
+++ ehrgonomic/urls.py
@@ -6,13 +6,13 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'ehrgonomic.views.home', name='home'),
+    url(r'^$', 'ehrgonomic.views.home', name='home'),
     # url(r'^ehrgonomic/', include('ehrgonomic.foo.urls')),
-    url(r'^sample/', 'records.views.sample', name='sample'),
-    url(r'^old_sample/', 'records.views.old_sample', name='old_sample'),
+    #url(r'^sample/', 'records.views.sample', name='sample'),
+    #url(r'^old_sample/', 'records.views.old_sample', name='old_sample'),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
 )
